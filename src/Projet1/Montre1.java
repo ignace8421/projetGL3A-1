23d40ae2e5a0cfaf2c71f69a4cc151db0015c0a4
macmac <jeanfrancoisignaceessama@gmail.com>
2022-02-02 10:46:10 +0100

    initial commit

---
+++ src/Projet1/Montre1.java
@@ -0,0 +1,8 @@
+package Projet1;
+
+public class Montre1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("bonjour");
+    }
+}
